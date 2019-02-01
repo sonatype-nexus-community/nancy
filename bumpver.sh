d7aef32e628c61fe4b927da4ad4be7e5de4af0ad
Dan Rollo <danrollo@gmail.com>
2019-02-01 15:43:53 -0500

    more semver friendly version increment

---
+++ bumpver.sh
@@ -0,0 +1,15 @@
+export LAST_VERSION=$(git describe --abbrev=0 --tags)
+echo $LAST_VERSION
+
+export LAST_PREFIX=$(cut -d'.' -f1 -f2 <<< $LAST_VERSION)
+echo $LAST_PREFIX
+
+export LAST_SUFFIX=$(cut -d'.' -f3 <<< $LAST_VERSION)
+echo $LAST_SUFFIX
+
+export NEW_SUFFIX=$(expr "$LAST_SUFFIX" + 1)
+echo $NEW_SUFFIX
+
+export NEW_VERSION="$LAST_PREFIX.$NEW_SUFFIX"
+echo $NEW_VERSION
+
