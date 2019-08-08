b38a7fe5a70437a2f51d6bf054b2f416f9883f6d
Nick Cook <ncook@sonatype.com>
2019-08-08 14:26:24 -0400

    start of a landing page for nancy

---
+++ docs/_javascript/main.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Hello Bulma!');
+});
---
+++ docs/lib/main.js
@@ -0,0 +1,5 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('Hello Bulma!');
+});
