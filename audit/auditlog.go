b1ec4f9fe2a2f1ae0460db5d88a9679637f6081f
Nathan Zender <github@nathanzender.com>
2020-02-24 14:27:37 -0500

    Multi format output (#57)
    💥

--- audit/auditlog.go
+++ audit/auditlog.go
@@ -14,83 +14,53 @@
 package audit
 
 import (
-	"fmt"
-	"log"
-	"strconv"
-
-	"github.com/logrusorgru/aurora"
+	log "github.com/sirupsen/logrus"
+	"github.com/sonatype-nexus-community/nancy/buildversion"
 	"github.com/sonatype-nexus-community/nancy/types"
+	"os"
 )
-
-func LogInvalidSemVerWarning(noColor bool, quiet bool, invalidPurls []string) {
-	if !quiet {
-		packageCount := len(invalidPurls)
-		warningMessage := "!!!!! WARNING !!!!!\nScanning cannot be completed on the following package(s) since they do not use semver."
-		au := aurora.NewAurora(!noColor)
-		fmt.Println(au.Red(warningMessage))
-
-		for i := 0; i < len(invalidPurls); i++ {
-			idx := i + 1
-			purl := invalidPurls[i]
-			fmt.Println("["+strconv.Itoa(idx)+"/"+strconv.Itoa(packageCount)+"]", au.Bold(purl))
-		}
-		fmt.Println()
-	}
-}
-
-func logPackage(noColor bool, idx int, packageCount int, coordinate types.Coordinate) {
-	au := aurora.NewAurora(!noColor)
-	log.Println("["+strconv.Itoa(idx)+"/"+strconv.Itoa(packageCount)+"]",
-		au.Bold(coordinate.Coordinates),
-		au.Gray(20-1,"   No known vulnerabilities against package/version"))
-}
-
-func logVulnerablePackage(noColor bool, idx int, packageCount int, coordinate types.Coordinate) {
-	au := aurora.NewAurora(!noColor)
-	fmt.Println("------------------------------------------------------------")
-	fmt.Println("["+strconv.Itoa(idx)+"/"+strconv.Itoa(packageCount)+"]",
-		au.Bold(au.Red(coordinate.Coordinates+"  [Vulnerable]")),
-		"   "+strconv.Itoa(len(coordinate.Vulnerabilities)),
-		"known vulnerabilities affecting installed version")
-
-	for j := 0; j < len(coordinate.Vulnerabilities); j++ {
-		if !coordinate.Vulnerabilities[j].Excluded {
-			fmt.Printf("\n%s\n%s\n\nID:%s\nDetails:%s\n",
-				coordinate.Vulnerabilities[j].Title,
-				coordinate.Vulnerabilities[j].Description,
-				coordinate.Vulnerabilities[j].Id,
-				coordinate.Vulnerabilities[j].Reference)
-		}
-	}
-
-	return
-}
 
 // LogResults will given a number of expected results and the results themselves, log the
 // results.
-func LogResults(noColor bool, packageCount int, coordinates []types.Coordinate, exclusions []string) int {
+func LogResults(formatter log.Formatter, packageCount int, coordinates []types.Coordinate, invalidCoordinates []types.Coordinate, exclusions []string) int {
 	vulnerableCount := 0
 
 	for _, c := range coordinates {
 		c.ExcludeVulnerabilities(exclusions)
 	}
 
+	var auditedCoordinates []types.Coordinate
+	var vulnerableCoordinates []types.Coordinate
+
 	for i := 0; i < len(coordinates); i++ {
 		coordinate := coordinates[i]
-		idx := i + 1
-
-		if !coordinate.IsVulnerable() {
-			logPackage(noColor, idx, packageCount, coordinate)
-		} else {
-			logVulnerablePackage(noColor, idx, packageCount, coordinate)
+		if coordinate.IsVulnerable() {
 			vulnerableCount++
+			vulnerableCoordinates = append(vulnerableCoordinates, coordinate)
 		}
+		auditedCoordinates = append(auditedCoordinates, coordinate)
 	}
 
-	fmt.Println()
-	au := aurora.NewAurora(!noColor)
-	fmt.Println("Audited dependencies:", strconv.Itoa(packageCount)+",",
-		"Vulnerable:", au.Bold(au.Red(strconv.Itoa(vulnerableCount))))
+	if invalidCoordinates == nil {
+		invalidCoordinates = make([]types.Coordinate, 0)
+	}
+	if exclusions == nil {
+		exclusions = make([]string, 0)
+	}
+	if vulnerableCoordinates == nil {
+		vulnerableCoordinates = make([]types.Coordinate, 0)
+	}
+	log.SetFormatter(formatter)
+	log.SetOutput(os.Stdout)
+	log.WithFields(log.Fields{
+		"exclusions":     exclusions,
+		"num_audited":    packageCount,
+		"num_vulnerable": vulnerableCount,
+		"audited":        auditedCoordinates,
+		"vulnerable":     vulnerableCoordinates,
+		"invalid":        invalidCoordinates,
+		"version":        buildversion.BuildVersion,
+	}).Info("")
 
 	return vulnerableCount
 }
