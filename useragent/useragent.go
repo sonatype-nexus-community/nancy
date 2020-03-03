401bba3f467cbd4a7c1f36ad982b555c910d0c65
Jeffry Hesse <jhesse@sonatype.com>
2020-03-02 16:53:09 -0900

    Tests and the most popular cases, minus GH Actions

--- useragent/useragent.go
+++ useragent/useragent.go
@@ -1,3 +1,18 @@
+// Copyright 2020 Sonatype Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package useragent has functions for setting a user agent with helpful information
 package useragent
 
 import (
@@ -8,27 +23,51 @@ import (
 	"github.com/sonatype-nexus-community/nancy/buildversion"
 )
 
+var GOOS = runtime.GOOS
+var GOARCH = runtime.GOARCH
+
 func GetUserAgent() (useragent string) {
 	useragent = fmt.Sprintf("nancy-client/%s", buildversion.BuildVersion)
 	if checkForCIEnvironment() {
 		callerInfo := getCallerInfo()
-		if callerInfo == "" && checkForCircleCI() {
-			useragent = useragent + fmt.Sprintf(" (%s; %s %s)", "circleci", runtime.GOOS, runtime.GOARCH)
+		if callerInfo == "" {
+			useragent = checkCIEnvironments(useragent)
+			return
 		}
-		if callerInfo == "" && checkForBitBucket() {
-			useragent = useragent + fmt.Sprintf(" (%s; %s %s)", "bitbucket", runtime.GOOS, runtime.GOARCH)
-		} else {
-			useragent = useragent + fmt.Sprintf(" (%s; %s %s)", callerInfo, runtime.GOOS, runtime.GOARCH)
-		}
-	} else {
-		useragent = useragent + fmt.Sprintf(" (%s; %s %s)", "non ci usage", runtime.GOOS, runtime.GOARCH)
+		useragent = useragent + fmt.Sprintf(" (%s; %s %s)", callerInfo, GOOS, runtime.GOARCH)
+		return
 	}
+	useragent = useragent + fmt.Sprintf(" (%s; %s %s)", "non ci usage", GOOS, runtime.GOARCH)
 
 	return
 }
 
+func checkCIEnvironments(useragent string) string {
+	if checkForCISystem("CIRCLECI") {
+		useragent = useragent + fmt.Sprintf(" (%s; %s %s)", "circleci", GOOS, runtime.GOARCH)
+	}
+	if checkForCISystem("BITBUCKET_BUILD_NUMBER") {
+		useragent = useragent + fmt.Sprintf(" (%s; %s %s)", "bitbucket", GOOS, runtime.GOARCH)
+	}
+	if checkForCISystem("TRAVIS") {
+		useragent = useragent + fmt.Sprintf(" (%s; %s %s)", "travis-ci", GOOS, runtime.GOARCH)
+	}
+	if checkIfJenkins() {
+		useragent = useragent + fmt.Sprintf(" (%s; %s %s)", "jenkins", GOOS, runtime.GOARCH)
+	}
+	return useragent
+}
+
 func checkForCIEnvironment() bool {
 	s := os.Getenv("CI")
+	if s != "" {
+		return true
+	}
+	return checkIfJenkins()
+}
+
+func checkIfJenkins() bool {
+	s := os.Getenv("JENKINS_HOME")
 	if s != "" {
 		return true
 	}
@@ -38,22 +77,11 @@ func checkForCIEnvironment() bool {
 // Returns info from SC_CALLER_INFO, example: bitbucket-nancy-pipe-0.1.9
 func getCallerInfo() string {
 	s := os.Getenv("SC_CALLER_INFO")
-	if s != "" {
-		return s
-	}
-	return ""
+	return s
 }
 
-func checkForCircleCI() bool {
-	s := os.Getenv("CIRCLECI")
-	if s != "" {
-		return true
-	}
-	return false
-}
-
-func checkForBitBucket() bool {
-	s := os.Getenv("BITBUCKET_BUILD_NUMBER")
+func checkForCISystem(system string) bool {
+	s := os.Getenv(system)
 	if s != "" {
 		return true
 	}
--- useragent/useragent_test.go
+++ useragent/useragent_test.go
@@ -1,11 +1,107 @@
+// Copyright 2020 Sonatype Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package useragent has functions for setting a user agent with helpful information
 package useragent
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
-func TestGetUserAgent(t *testing.T) {
+func TestGetUserAgentNonCI(t *testing.T) {
+	expected := "nancy-client/development (non ci usage; linux amd64)"
+
+	GOOS = "linux"
+	GOARCH = "amd64"
+
 	agent := GetUserAgent()
 
-	expected := "nancy-client/build"
+	if agent != expected {
+		t.Errorf("User Agent not retrieved successfully, got %s, expected %s", agent, expected)
+	}
+}
+
+func TestGetUserAgentCircleCI(t *testing.T) {
+	expected := "nancy-client/development (circleci; linux amd64)"
+
+	os.Setenv("CI", "true")
+	os.Setenv("CIRCLECI", "true")
+	GOOS = "linux"
+	GOARCH = "amd64"
+
+	agent := GetUserAgent()
+
+	if agent != expected {
+		t.Errorf("User Agent not retrieved successfully, got %s, expected %s", agent, expected)
+	}
+}
+
+func TestGetUserAgentJenkins(t *testing.T) {
+	expected := "nancy-client/development (jenkins; linux amd64)"
+
+	os.Setenv("JENKINS_HOME", "/a/place/under/the/sun")
+	GOOS = "linux"
+	GOARCH = "amd64"
+
+	agent := GetUserAgent()
+
+	if agent != expected {
+		t.Errorf("User Agent not retrieved successfully, got %s, expected %s", agent, expected)
+	}
+}
+
+func TestGetUserAgentTravisCI(t *testing.T) {
+	expected := "nancy-client/development (travis-ci; linux amd64)"
+
+	os.Setenv("CI", "true")
+	os.Setenv("TRAVIS", "true")
+	GOOS = "linux"
+	GOARCH = "amd64"
+
+	agent := GetUserAgent()
+
+	if agent != expected {
+		t.Errorf("User Agent not retrieved successfully, got %s, expected %s", agent, expected)
+	}
+}
+
+func TestGetUserAgentBitBucket(t *testing.T) {
+	expected := "nancy-client/development (bitbucket; linux amd64)"
+
+	os.Setenv("CI", "true")
+	os.Setenv("BITBUCKET_BUILD_NUMBER", "20")
+	GOOS = "linux"
+	GOARCH = "amd64"
+
+	agent := GetUserAgent()
+
+	if agent != expected {
+		t.Errorf("User Agent not retrieved successfully, got %s, expected %s", agent, expected)
+	}
+}
+
+func TestGetUserAgentScCallerInfo(t *testing.T) {
+	expected := "nancy-client/development (bitbucket-nancy-pipe-0.1.9; linux amd64)"
+
+	os.Setenv("CI", "true")
+	os.Setenv("SC_CALLER_INFO", "bitbucket-nancy-pipe-0.1.9")
+	GOOS = "linux"
+	GOARCH = "amd64"
+
+	agent := GetUserAgent()
+
 	if agent != expected {
 		t.Errorf("User Agent not retrieved successfully, got %s, expected %s", agent, expected)
 	}
