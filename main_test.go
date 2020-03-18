06514c41309d3db832ad291880a880345b287d5f
Dan Rollo <danrollo@gmail.com>
2020-03-18 18:55:41 -0400

    date up

--- main_test.go
+++ main_test.go
@@ -1,4 +1,4 @@
-// Copyright 2018 Sonatype Inc.
+// Copyright 2020 Sonatype Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
