48a1a82bf365a5e5c97b1e23844227f036af1c94
Dan Rollo <danrollo@gmail.com>
2020-04-20 11:55:39 -0400

    Fix seg fault when Err struct member was nil. So much for making Jeffry NOT code on the weekend.

--- customerrors/error_test.go
+++ customerrors/error_test.go
@@ -18,20 +18,17 @@ package customerrors
 
 import (
 	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
-type ErrorExit struct {
-	Message  string
-	Err      error
-	ExitCode int
-}
-
-func (ee ErrorExit) Error() string {
-	var errString string
-	if ee.Err != nil {
-		errString = ee.Err.Error()
-	} else {
-		errString = "nil"
-	}
-	return fmt.Sprintf("exit code: %d - %s - error: %s", ee.ExitCode, ee.Message, errString)
+func TestError(t *testing.T) {
+	assert.Equal(t, "exit code: 2 - MyMessage - error: MyError",
+		ErrorExit{Message: "MyMessage", Err: fmt.Errorf("MyError"), ExitCode: 2}.Error())
+	assert.Equal(t, "exit code: 0 - MyMessage - error: MyError",
+		ErrorExit{Message: "MyMessage", Err: fmt.Errorf("MyError")}.Error())
+	assert.Equal(t, "exit code: 0 - MyMessage - error: nil",
+		ErrorExit{Message: "MyMessage"}.Error())
+	assert.Equal(t, "exit code: 0 -  - error: nil",
+		ErrorExit{}.Error())
 }
